e1ae04e55917c2b33e3921c7647db745c615006f
Vital24 <zynn00001@gmail.com>
2023-03-10 10:16:30 -0500

    List

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,32 +1,9 @@
 <script setup>
-// import { RouterLink, RouterView } from "vue-router";
-// import HelloWorld from "./components/HelloWorld.vue";
 import Menu from "./components/Menu.vue";
 </script>
 
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="@/assets/logo.svg"
-      width="125"
-      height="125"
-    />
-    <div>
-      <Menu></Menu>
-    </div>
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <Menu></Menu>
 </template>
 
 <style scoped>
