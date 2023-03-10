7f34f92a58df7fb9780fbeb834086e9853fd335d
Vital24 <113121145+Vital24@users.noreply.github.com>
2023-03-09 20:00:40 -0500

    A

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,6 +1,6 @@
 <script setup>
-import { RouterLink, RouterView } from "vue-router";
-import HelloWorld from "./components/HelloWorld.vue";
+// import { RouterLink, RouterView } from "vue-router";
+// import HelloWorld from "./components/HelloWorld.vue";
 import Menu from "./components/Menu.vue";
 </script>
 
--- vue/src/components/Menu.vue
+++ vue/src/components/Menu.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
-    <h1 v-for="Food in Foods" :key="Food">{{ Food }}</h1>
+    <h1 v-for="Food in ThaiFood" :key="Food">
+      {{ Food.dish }} Price: {{ Food.price }}
+    </h1>
   </div>
 </template>
 
@@ -8,37 +10,38 @@
 export default {
   data() {
     return {
-      Foods: [
-        "Som Tam",
-        "Laab",
-        "Jok",
-        "Pad Thai",
-        "Pad See Ew",
-        "Kuay Teow Neua",
-        "Ba Mee Kiew",
-        "Khao Niao",
-        "Gai Yang",
-        "Pla Plao",
-        "Gai Hor Bai Toey",
-        "Sai Oua",
-        "Hor Mok Pla",
-        "Hoi Tod",
-        "Pad Kra Pao",
-        "Khao Kha Moo",
-        "Khao Kluk Kapi",
-        "Khao Mok Gai",
-        "Khao Pad",
-        "Phrik Kaeng Phet",
-        "Kaeng Kari",
-        "Kaeng Khiao Wan",
-        "Khao Soi",
-        "Tom Yum",
-        "Tom Kha Gai",
-        "Kaeng Som",
-        "Massaman Curry",
-        "Panang Curry",
-        "Khao Niao Mamuang",
-        "Kluai Buat Chi",
+      ThaiFood: [
+        { dish: "Pad Thai", price: 2.0 },
+        { dish: "Tom Yum Goong", price: 3.0 },
+        { dish: "Green Curry", price: 4.0 },
+        { dish: "Massaman Curry", price: 5.0 },
+        { dish: "Som Tum", price: 2.5 },
+        { dish: "Papaya Salad", price: 2.5 },
+        { dish: "Pad Kra Pao", price: 3.5 },
+        { dish: "Khao Pad", price: 3.0 },
+        { dish: "Tom Kha Gai", price: 3.5 },
+        { dish: "Gaeng Daeng", price: 4.5 },
+        { dish: "Gaeng Keow Wan Kai", price: 4.0 },
+        { dish: "Pla Rad Prik", price: 7.0 },
+        { dish: "Pad See Ew", price: 3.5 },
+        { dish: "Mango Sticky Rice", price: 2.5 },
+        { dish: "Kai Med Ma Muang", price: 4.0 },
+        { dish: "Tom Yam Kung", price: 3.0 },
+        { dish: "Laab Moo", price: 3.5 },
+        { dish: "Pad Krapow Moo Saap", price: 3.5 },
+        { dish: "Khao Man Gai", price: 2.5 },
+        { dish: "Khao Niew Mamuang", price: 2.5 },
+        { dish: "Gai Yang", price: 3.0 },
+        { dish: "Pla Pao", price: 8.0 },
+        { dish: "Gaeng Som", price: 4.0 },
+        { dish: "Pad Prik King", price: 3.5 },
+        { dish: "Pad Pak Boong", price: 3.0 },
+        { dish: "Rad Na", price: 3.5 },
+        { dish: "Kao Soi", price: 4.0 },
+        { dish: "Pad Siew", price: 3.5 },
+        { dish: "Kai Jeow", price: 2.0 },
+        { dish: "Khao Kha Moo", price: 2.5 },
+        { dish: "Kai Pad Med Mamuang", price: 4.0 },
       ],
     };
   },
