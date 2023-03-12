b40ad1bf4ebe48d693f41809aeb0d74d5a3f599c
Vital24 <113121145+Vital24@users.noreply.github.com>
2023-03-12 19:54:14 -0400

    DD

---
+++ vue/src/components/Socials.vue
@@ -0,0 +1,17 @@
+<template>
+  <img
+    @click="target = `blank`"
+    src="https://cdn-icons-png.flaticon.com/512/174/174855.png"
+    alt="Instagram"
+  />
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped></style>
